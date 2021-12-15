874a378cae241fe36b422753b6e1f59849d6bf1b
Biswesh Mohapatra <biswesh.mohapatra@iiitb.org>
2021-12-15 06:10:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import sys
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForSequenceClassification
 from scipy.special import softmax
 
-topics_list = ['weed', 'abortion']
+topics_list = ['weed', 'gene']
+log = []
 
 class blenderBot:
     def __init__(self):
@@ -26,15 +27,75 @@ class opinionClassifier:
         self.model = AutoModelForSequenceClassification.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis")
         self.tokenizer = AutoTokenizer.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis")
 
-    def get_opinion(self, user_query, previous_belief):
+    def get_opinion(self, user_query):
+        if "not sure" in user_query or "dont know" in user_query or "don't know" in user_query:
+            return ["neutral", "against"]
+
         inputs = self.tokenizer([user_query], return_tensors='pt')
         output = self.model(**inputs)
         results = softmax(output[0][0].detach().numpy())
 
         # 0 - negative 1 - neutral 2 - positive
-         
+        if results[0] > 0.5:
+            return ["against"]
+        elif results[2] > 0.25 and results[0] < 0.1:
+            return ["for"]
+        else:
+            if results[0] > 0.25:
+                return ["neutral", "against"]
+            else:
+                return ["neutral", "for"]
 
-def socratic_chatbot(current_topic, topic_idx, user_name):
+    def yes_no_detector(self, user_query):
+        if any(x in ["no", "nope", "not", "don't"] for x in user_query.split()):
+            return "no"
+        else:
+            return "yes"
+
+def detect_reason(response, prev_user_response, p, q):
+    if '[P]' in response and '[Q]' not in response:
+        if 'because' in prev_user_response:
+            p = prev_user_response.split('because')[-1]
+            return p, '', response.replace('[P]', p).strip() 
+        
+        elif 'since' in prev_user_response:
+            p = prev_user_response.split('since')[-1]
+            return p, '', response.replace('[P]', p).strip() 
+
+        elif ' as ' in prev_user_response:
+            p = prev_user_response.split(' as ')[-1]
+            return p, '', response.replace('[P]', p).strip() 
+
+        elif 'that' in prev_user_response:
+            p = prev_user_response.split('that')[-1]
+            return p, '', response.replace('[P]', p).strip() 
+        else:
+            p = prev_user_response
+            return p, '', response.replace('[P]', p).strip() 
+
+    elif '[Q]' in response:
+        if 'because' in prev_user_response:
+            q = prev_user_response.split('because')[-1]
+            return p, q, response.replace('[P]', p).replace('[Q]', q).strip() 
+        
+        elif 'since' in prev_user_response:
+            q = prev_user_response.split('since')[-1]
+            return p, q, response.replace('[P]', p).replace('[Q]', q).strip() 
+
+        elif ' as ' in prev_user_response:
+            q = prev_user_response.split(' as ')[-1]
+            return p, q, response.replace('[P]', p).replace('[Q]', q).strip() 
+
+        elif 'that' in prev_user_response:
+            q = prev_user_response.split('that')[-1]
+            return p, q, response.replace('[P]', p).replace('[Q]', q).strip() 
+        else:
+            q = prev_user_response
+            return p, q, response.replace('[P]', p).replace('[Q]', q).strip() 
+            
+        
+
+def socratic_chatbot(current_topic, user_name, current_state='state_0'):
     json_file = current_topic + '.json'
     
     with open('./FSM/'+json_file) as f:
@@ -42,16 +103,64 @@ def socratic_chatbot(current_topic, topic_idx, user_name):
 
     opinion_classifier = opinionClassifier()
     context = []
-    current_state = 'state_0'
     current_belief = ''
+    reason_p = ''
+    reason_q = ''
+    user_response = ''
+
     while True:
         state = fsm[current_state]
-        print('Mark : ' + state['response'])
-        response = input(user_name + " : ")
-        current_belief = opinion_classifier.get_belief(response, current_belief)
-        current_state = state['next_state'][current_belief]
+        response = state['Response'].replace("[NAME]", user_name)
+        reason_p, reason_q, response = detect_reason(response, user_response, reason_p, reason_q)
 
-def normal_chatbot(current_topic, topic_idx, user_name):
+        time.sleep(len(response.split())*0.2)
+
+        print('Mark : ' + response)
+        user_response = input(user_name + " : ")
+
+        if 'Next_State' in state:
+            next_state = state['Next_State']
+            if isinstance(next_state, str):
+                if "change" in next_state:
+                    current_state = next_state[7:]
+                    
+                    if current_topic == "weed":
+                        current_topic = "gene"
+                    else:
+                        current_topic = "weed"
+                    
+                    socratic_chatbot(current_topic, user_name, current_state)
+                    break
+                else:
+                    current_state = next_state
+            else:
+                # If next state depends on opinion
+                if "for" in next_state:
+                    current_belief = opinion_classifier.get_opinion(user_response)
+                    if current_belief[0] == "for":
+                        current_state = next_state["for"]
+                    elif current_belief[0] == "against":
+                        current_state = next_state["against"]
+                    else:
+                        if "neutral" in next_state:
+                            current_state = next_state["neutral"]
+                        else:
+                            current_state = next_state[current_belief[1]]
+
+                # If next state depends upon yes or no type of answer
+                elif "yes" in next_state:
+                    current_belief = opinion_classifier.yes_no_detector(user_response)
+                    current_state = next_state[current_belief]
+                else:
+                    print("Some Error!!!")
+                    break
+        else:
+            # End of FSM
+            break
+
+def normal_chatbot(current_topic, user_name, current_state='state_0'):
+    # start using an open-domain dialog model for response generation
+    blender_bot = blenderBot()
     json_file = current_topic + '.json'
 
     with open('./FSM/'+json_file) as f:
@@ -60,37 +169,45 @@ def normal_chatbot(current_topic, topic_idx, user_name):
     opinion_classifier = opinionClassifier()
     turn = 0
     context = []
-    while turn < 2:
-        print('Mark : Hey champ!!! How was the day?')
-        response = input(user_name + " : ")
-        turn += 1
+    while True:
+        state = fsm[current_state]
+        response = state['Response'].replace("[NAME]", user_name)
+        time.sleep(len(response.split())*0.2)
+        print('Mark : ' + response)
+        user_response = input(user_name + " : ")
+
+        if current_state == "state_3":
+            break
+        
+        current_state = state["Next_State"]
 
     # response to move away from topic
-    diverging_sentence = fsm['diverging statement']
+    diverging_sentence = fsm['state_diverging_statement']["Response"]
     print('Mark : ' + diverging_sentence)
     response = input(user_name + " : ")
     context.append(diverging_sentence)
     context.append(response)
     turn += 1
 
-    # start using an open-domain dialog model for response generation
-    blender_bot = blenderBot()
-
-    while turn < 8:
+    while turn < 7:
         model_response = blender_bot.get_response(" ".join(context))
+        print('Mark : ' + model_response)
         user_response = input(user_name + " : ")
         context.append(model_response)
         context.append(user_response)
+
     
+    print('Mark : Anyways, it was nice to talk to you! I got to go now. See you soon... Bye!!')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='chatbot for socratc questioning')
-    parser.add_argument('--group_type', type=int, default=0, metavar='N', help='conditional group(0 for socratic chatbot, 1 for normal chatbot)')
+    parser.add_argument('--group', type=int, default=0, metavar='N', help='conditional group(0 for socratic chatbot, 1 for normal chatbot)')
     parser.add_argument('--name', type=str, default='', help='get name of the participant')
     args = parser.parse_args()
     
     topic_idx = random.randint(0, len(topics_list)-1)
-    current_topic = topics_list[topic_idx]
+    current_topic = topics_list.pop(topic_idx)
 
     # Check name of the participant
     if args.name == '':
@@ -100,10 +217,10 @@ if __name__ == "__main__":
         user_name = args.name
 
     # Check the group type
-    if args.group_type == 0:
-        socratic_chatbot(current_topic, topic_idx, user_name)
-    elif args.group_type == 1:
-        normal_chatbot(current_topic, topic_idx, user_name)
+    if args.group == 0:
+        socratic_chatbot(current_topic, user_name)
+    elif args.group == 1:
+        normal_chatbot(current_topic, user_name)
     else:
         print('Please type correct group type!!!')
     
